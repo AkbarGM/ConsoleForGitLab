dbaf431de8578003bb82e3bab9fc7933930d4aa2
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-09-18 13:22:16 +0500

    Add project files.

---
+++ ConsoleForGitLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleForGitLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
